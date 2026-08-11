a3d017258e2e85646fe73fcf8804aba9bd2b4b40
wuyu <wuyu@burnergame.com>
2026-08-11 21:33:18 +0800

    chore: 补充遗漏的配置更新、空目录清理和 BOM 编码规范化

--- Assets/Plugins/Sirenix/Odin Inspector/Modules/Unity.Mathematics/MathematicsDrawers.cs
+++ Assets/Plugins/Sirenix/Odin Inspector/Modules/Unity.Mathematics/MathematicsDrawers.cs
@@ -1,4 +1,4 @@
-//-----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
 // <copyright file="MathematicsDrawers.cs" company="Sirenix ApS">
 // Copyright (c) Sirenix ApS. All rights reserved.
 // </copyright>
