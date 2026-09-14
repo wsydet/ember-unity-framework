5092e032c79a6a5835ba919a16e3ecad125214ac
wuyu <wuyu@burnergame.com>
2026-09-14 20:06:34 +0800

    chore: 同步 UPM 脚本编码与解决方案顺序

--- Packages/com.ember/UPMManager/Editor/EmberUPMReleaseNotes.cs
+++ Packages/com.ember/UPMManager/Editor/EmberUPMReleaseNotes.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Ember Unity Framework. All rights reserved.
+﻿// Copyright (c) 2026 Ember Unity Framework. All rights reserved.
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
